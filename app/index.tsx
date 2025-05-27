c57b19dd23bfd30f78c2c4953cde39048d9ff542
Kim CHOUARD <github@chouard.kim>
2025-05-27 09:27:39 +0200

    Init Expo Router project

---
+++ app/index.tsx
@@ -0,0 +1,46 @@
+import { useAudioPlayer } from 'expo-audio';
+import { Image } from 'expo-image';
+import { Pressable, StyleSheet, View } from 'react-native';
+
+const chevreImage = require('../assets/images/chevre_de_verzasca.jpg');
+const chevreSound = require('../assets/audio/chevre.mp3');
+
+export default function HomeScreen() {
+  const player = useAudioPlayer(chevreSound);
+
+  const playSound = async () => {
+    try {
+      await player.play();
+    } catch (error) {
+      console.error("Failed to play sound:", error);
+      // Handle error (e.g., show an alert to the user)
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={playSound} style={styles.touchable}>
+        <Image
+          source={chevreImage}
+          style={styles.image}
+          contentFit="cover"
+        />
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  touchable: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
