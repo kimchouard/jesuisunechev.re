234502937d61514918faac47694608d6dc5d52d7
Kim CHOUARD <github@chouard.kim>
2025-05-27 14:38:11 +0200

    Reanimated example. WRONG => bad timing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="goat-reanimated" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
